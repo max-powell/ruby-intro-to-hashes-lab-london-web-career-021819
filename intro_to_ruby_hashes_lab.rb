b3b6a768e7a78aac5144f4a8924e8c160741d60a
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-31 11:59:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,8 +12,8 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {}}
-	monopoly[:railroads][:pieces] = 4
+	monopoly = {railroads: {pieces: 4, names: {}, rent_in_dollars: {}}}
+
 end
 
 def monopoly_with_third_tier
